a320922f8cfe0f6605779fc4d22276731aee5664
Snail <xu.jin.1994@163.com>
2017-11-16 20:05:19 +0800

    增加了文字

--- src/main/java/com/siemens/oa/OaApplication.java
+++ src/main/java/com/siemens/oa/OaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OaApplication.class, args);
-		System.out.println(" ");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OaApplication.class, args);
+        System.out.println("徐进");
+    }
 }
